e465dc8f49000e25844427014fc25e92f46a787f
fajrulfalah93 <sunrise.pacet@gmail.com>
2023-01-25 11:08:25 +0700

    ubah favicon

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
         <title>UKPBJ Kab. Mojokerto</title>
         <meta name="description" content="Aplikasi PBJ" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="http://localhost:3000/favicon.ico" />
       </Head>
       <main>
         <div>
